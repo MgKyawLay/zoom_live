a1d0786d684edfbffd353fa6ffec651ee270bcfe
Kmk <56061215+MgKyawLay@users.noreply.github.com>
2024-10-11 15:42:30 +0630

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-
+import React, { useState } from "react"; // Import useState
 import "./App.css";
 import uitoolkit from "@zoom/videosdk-ui-toolkit";
 import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 
 function App() {
-  const handleJoin = () => {
-    const token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBfa2V5IjoiVnEzYjdsUmhSdDdsTklLYjF3NlB6R0hqRU1odm9hMFAzVWFxIiwidHBjIjoiand0IHRlc3QiLCJyb2xlX3R5cGUiOjEsInNlc3Npb25fa2V5Ijoiand0IHRlc3Q1MjA0IiwidXNlcl9pZGVudGl0eSI6IkRhdyBUaW4gTmlsYXIgVHVuIiwidmVyc2lvbiI6MSwiaWF0IjoxNzI3MTUxNzEzLCJleHAiOjE3MjcxNTg5MTN9.jlQ1H3XIvfYsEprPpVtwAwbfp1E6FCu5uVdHxPtcbRE";
+  const [token, setToken] = useState("");
+  const [sessionName, setSessionName] = useState("");
+
+
+  const handleJoin = () => {  
     var config = {
       videoSDKJWT: token,
-      sessionName: "jwt test",
-      userName: "UserA",
+      sessionName: sessionName, 
+      userName: "Instructor",
       sessionPasscode: "abc123",
-      features: ["video", "audio", "share", "chat", "users", "settings"],
+      features: ["video", "audio", "share", "chat", "users", "settings", "preview"],
+      options: { init: {}, audio: {}, video: {}, share: {}},
     };
     const sessionContainer = document.getElementById("sessionContainer");
     uitoolkit.joinSession(sessionContainer, config);
   };
+
   return (
     <div className="App">
       <div id="sessionContainer"></div>
-      <button onClick={handleJoin}>join</button>
+      <div className="inputContainer">
+        <input
+          type="text"
+          value={token}
+          onChange={(e) => setToken(e.target.value)} // Update state on token input change
+          placeholder="Enter JWT token"
+        />
+        <input
+          type="text"
+          value={sessionName}
+          onChange={(e) => setSessionName(e.target.value)} // Update state on session name input change
+          placeholder="Enter session name"
+        />
+        <button onClick={handleJoin}>Join</button>
+      </div>
     </div>
   );
 }
